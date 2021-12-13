f4dd0dea78b78893f465acf0f6769e36c448f684
Pavan-Kiran-Chidirala <pavankiran.chidirala@gmail.com>
2021-12-13 07:21:19 +0000

    message

---
+++ src/components/LanguageFilterItem/index.js
@@ -0,0 +1,24 @@
+// Write your code here
+import './index.css'
+
+const LanguageFilterItem = props => {
+  const {itemDetails, clickAction, activeId} = props
+  const {id, language} = itemDetails
+  const classAdd = id === activeId ? 'color-item' : ''
+  const clickEvent = () => {
+    clickAction(id)
+  }
+  return (
+    <li className="list-item">
+      <button
+        type="button"
+        className={`list-button ${classAdd}`}
+        onClick={clickEvent}
+      >
+        {language}
+      </button>
+    </li>
+  )
+}
+
+export default LanguageFilterItem
